3626e456448a0546bcec93b69f96932c7fdc7083
sutson <aberdeenrules@hotmail.com>
2026-05-18 03:02:41 +0100

    add charge mode, ability to see connected users on home screen, various code cleanups

--- app.js
+++ app.js
@@ -13,23 +13,66 @@ const io = new Server(server, {
 });
 
 const userMap = new Map();
+
 let buzzInfo = [];
 let idkList = [];
+let userList = [];
+
+const MAX_CHARGES = 3;
+let isChargeModeEnabled = false;
+let userChargeData = new Object();
+
 let userInfo = new Object();//Each user has properties, and is stored as a property of userInfo
 let anyObjections = false;
-const teamsList = ["Players","Chasers"]
-const teamsScore = [0,0];
+const teamsList = ["Players", "Chasers"]
+const teamsScore = [0, 0];
 let currentTeamNumber = 0;
+let sockets = []
+
+async function resetApp() {
+  console.log("resetting app");
+  io.emit("resetApp");
+  Object.keys(options).forEach(x => delete options[x]);
+  userMap.clear();
+  buzzInfo.splice(0, buzzInfo.length);
+  idkList.splice(0, buzzInfo.length);
+  Object.keys(userMap).forEach(x => delete userMap[x]);
+  Object.keys(userInfo).forEach(x => delete userInfo[x]);
+  anyObjections = false;
+  teamsList.splice(0, buzzInfo.length, "Players", "Chasers");
+  teamsScore.splice(0, teamsScore.length, 0, 0);
+  currentTeamNumber = 0;
+
+  const oldSockets = sockets;
+  sockets = [];
+
+  await new Promise(ok => setTimeout(ok, 1000));
+  oldSockets.forEach(x => x.disconnect(true));
+  oldSockets.splice(0, oldSockets.length);
+}
+
+// finds a user with zero charges left on current team, if they exist
+function getUserOnZeroCharges(teamName) {
+  const zeroChargeUserOnCurrentTeam = Object.entries(userChargeData).find(([_, chargeData]) => chargeData.teamName == teamName && chargeData.charges == 0)?.[0];
+  return zeroChargeUserOnCurrentTeam ? getUserId(zeroChargeUserOnCurrentTeam) : "";
+}
+
+function getUserId(userName) {
+  return userName.replaceAll(" ", "_");
+}
+
 io.on("connection", (socket) => {
   console.log("a user connected");
+  sockets.push(socket);
 
   socket.on("disconnect", () => {
-      console.log("user disconnected");
+    console.log("user disconnected");
   });
 
-  //initial info on connection
-  socket.emit("gameStateToClient", teamsList[currentTeamNumber], teamsScore[currentTeamNumber]);
-  
+  // initial info on connection
+  socket.emit("gameStateToClient", teamsList[currentTeamNumber], teamsScore[currentTeamNumber], isChargeModeEnabled, userChargeData);
+  socket.emit("getChargeModeState", isChargeModeEnabled);
+
   if (anyObjections == true) {
     socket.emit("objectionToClient", "????");
   }
@@ -39,73 +82,132 @@ io.on("connection", (socket) => {
   });
 
   socket.on("buzzerPressed", (newUserName, newTimeStamp) => {
-    if (idkList.includes(newUserName)) { 
+    if (idkList.includes(newUserName)) {
       const userIndex = idkList.indexOf(newUserName);
       idkList.splice(userIndex, 1)
     }
 
-    let firstBuzzTimeStamp = (function() { 
+    let firstBuzzTimeStamp = (() => {
       if (buzzInfo.length == 0) {
         return newTimeStamp
       }
       else {
         return buzzInfo[0].timeStamp;
       }
-    })();
-    buzzInfo.push({ 
+    });
+
+    buzzInfo.push({
       userName: newUserName,
       timeStamp: newTimeStamp,
-      lateTime: newTimeStamp-firstBuzzTimeStamp,
-      buzzOrder: buzzInfo.length+1
+      lateTime: newTimeStamp - firstBuzzTimeStamp,
+      buzzOrder: buzzInfo.length + 1
     });
+
     io.emit("buzzInfoToClient", buzzInfo);
   });
 
   socket.on("idkButtonPressed", (newUserName) => {
-    if (!idkList.includes(newUserName) && userInfo[newUserName] != null && userInfo[newUserName].teamName == teamsList[currentTeamNumber]) {
+    if (!idkList.includes(newUserName) && userInfo[newUserName] != null && (userInfo[newUserName].teamName == teamsList[currentTeamNumber])) {
       idkList.push(newUserName);
-      let teamSize = 0;
-      for (const user in userInfo) {
-        if (userInfo[user].teamName == userInfo[newUserName].teamName) {
-          teamSize++;
-        }
-      }
+      const currentTeamName = userInfo[newUserName].teamName;
+      const isUserLocked = getUserOnZeroCharges(teamsList[currentTeamNumber]); // determine if a user is locked out of buzzing
+      
+      let teamSize = Object.values(userInfo).filter((user) => user.teamName == currentTeamName).length;
+      if (isUserLocked) { teamSize--; } // reduce team size by 1 if user locked out, simulates "one less player" on the team
+
       io.emit("idkListToClient", idkList);
+
       if (idkList.length >= teamSize) {
         io.emit("passToClient", teamsList[currentTeamNumber]);
       }
     }
   });
- 
-  socket.on("scoresToServer", score => {
+
+  socket.on("scoresToServer", (score) => {
+    const currentTeamName = teamsList[currentTeamNumber]
     teamsScore[currentTeamNumber] += score;
+
+    const userBuzzed = buzzInfo.filter((user) => user.buzzOrder == 1)[0]
+    if (userBuzzed) { userChargeData[userBuzzed.userName].charges--; } // decrement charges by 1 for user who buzzed in
+
     if (teamsScore[currentTeamNumber] >= 2) {
       teamsScore[currentTeamNumber] = 0;
-      currentTeamNumber = (currentTeamNumber+1)%2;
+      currentTeamNumber = (currentTeamNumber + 1) % 2;
     }
+
     buzzInfo = [];
     idkList = [];
-    io.emit("gameStateToClient", teamsList[currentTeamNumber], teamsScore[currentTeamNumber]);
-    io.emit("clearBuzzers", teamsList[currentTeamNumber]);
+
+    // add 1 charge and remove any buzzer lockout for every user on the team who did not buzz
+    for (const user in userChargeData) {
+      const userEntry = userChargeData[user];
+      if ((userBuzzed?.userName != user) && userEntry.teamName == currentTeamName && userEntry.charges < MAX_CHARGES) {
+        userEntry.charges++;
+        io.emit("removeBuzzerLockouts", getUserId(user));
+      }
+    }
+
+    const userToLock = getUserOnZeroCharges(teamsList[currentTeamNumber]); // determine which user's buzzer should be locked, if any
+
+    io.emit("gameStateToClient", teamsList[currentTeamNumber], teamsScore[currentTeamNumber], isChargeModeEnabled, userChargeData);
+    io.emit("clearBuzzers", teamsList[currentTeamNumber], isChargeModeEnabled, userChargeData);
+    io.emit("addBuzzerLockouts", teamsList[currentTeamNumber], userToLock); // add lockout to user
   });
 
-  socket.on("updateUserInfo", (userName, teamName, buzzerId) => {
+  socket.on("updateUserInfo", (userName, teamName, buzzerId, isChargeMode) => {
     userInfo[userName] = {
       teamName: teamName,
       buzzerId: buzzerId
     }
-    io.emit("userInfoToClient", userInfo);
+
+    // if user doesn't already exist in userList, add them
+    if (!userList.some((item) => item.userName == userName)) {
+      const user = { userName: userName, teamName: teamName, buzzerId: buzzerId }
+      userList.push(user);
+    }
+    
+    // update user in userList if buzzer has changed (shows on the home screen)
+    const userBuzzerToUpdate = userList.find((user) => user.userName == userName && user.buzzerId != buzzerId);
+    if (userBuzzerToUpdate) { userBuzzerToUpdate.buzzerId = buzzerId; }
+    
+    io.emit("getUserList", userList); // emit userList for home screen
+
+    // create new entry for storing user charge data if none exists for current user
+    if (!userChargeData.hasOwnProperty(userName)) {
+      userChargeData[userName] = { teamName: teamName, charges: MAX_CHARGES };
+    }
+
+    io.emit("userInfoToClient", userInfo, isChargeMode, userChargeData);
+    if (!isChargeMode) { return; }
+    io.emit("addBuzzerLockouts", currentTeamNumber, getUserOnZeroCharges(teamsList[currentTeamNumber]));
   });
 
   //used by the host when they refresh the page.
-  socket.on("refreshUserInfo", () => {
-    io.emit("userInfoToClient", userInfo);
+  socket.on("refreshUserInfo", (isChargeMode) => {
+    io.emit("userInfoToClient", userInfo, isChargeMode, userChargeData);
+    if (!isChargeMode) { return; }
+    io.emit("addBuzzerLockouts", currentTeamNumber, getUserOnZeroCharges(teamsList[currentTeamNumber]));
   });
 
   socket.on("unregisterUsers", () => {
     userInfo = new Object();
-    io.emit("userInfoToClient", userInfo);
+    userChargeData = new Object();
+    userList = [];
+    io.emit("userInfoToClient", userInfo, isChargeModeEnabled, userChargeData);
     io.emit("reconnectUsers");
+    io.emit("getUserList", userList);
+  });
+
+  socket.on("toggleChargeMode", (isChargeMode) => {
+    userInfo = new Object();
+    userChargeData = new Object();
+    userList = [];
+    buzzInfo = [];
+    idkList = [];
+    isChargeModeEnabled = isChargeMode;
+    io.emit("userInfoToClient", userInfo, isChargeModeEnabled, userChargeData);
+    io.emit("reconnectUsers");
+    io.emit("getUserList", userList);
   });
 
   socket.on("objectionToServer", (userName) => {
@@ -119,35 +221,71 @@ io.on("connection", (socket) => {
     anyObjections = false;
     io.emit("clearObjectionToClient");
   });
+
+  socket.on("getCurrentTeam", () => {
+    io.emit("currentTeam", currentTeamNumber, buzzInfo, userChargeData)
+  });
+
+  // used to populate user lists on the home page
+  socket.on("onHomepageLoad", () => {
+    io.emit("getUserList", userList);
+  })
 });
 
 // const io = new Server(server);
 const port = 8080;
 server.listen(port);
 
-app.get("/", (request, response) => {
-  response.sendFile("pages/home.html", {root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("pages/home.html", { root: __dirname });
 });
 
-app.get("/host", (request, response) => {
-  response.sendFile("pages/host.html", {root: __dirname });
+app.get("/host", (req, res) => {
+  if (isChargeModeEnabled) {
+    return res.redirect("/host-charges" + req.url.replace("/host", ""));
+  }
+  res.sendFile("pages/host.html", { root: __dirname });
 });
 
-app.get("/play", (request, response) => {
-  response.sendFile("pages/play.html", {root: __dirname });
+// redirect host to host-charges if charge mode is enabled
+app.get("/host-charges", (req, res) => {
+  if (!isChargeModeEnabled) {
+    return res.redirect("/host" + req.url.replace("/host-charges", ""));
+  }
+  res.sendFile("pages/host.html", { root: __dirname });
 });
 
-app.get("/browserFunctions.js", (request, response) => {
-  response.sendFile("/browserFunctions.js", {root: __dirname });
+app.get("/play", (req, res) => {
+  if (isChargeModeEnabled) {
+    return res.redirect("/play-charges" + req.url.replace("/play", ""));
+  }
+  res.sendFile("pages/play.html", { root: __dirname });
+});
+
+// redirect users to play-charges if charge mode is enabled
+app.get("/play-charges", (req, res) => {
+  if (!isChargeModeEnabled) {
+    return res.redirect("/play" + req.url.replace("/play-charges", ""));
+  }
+  res.sendFile("pages/play.html", { root: __dirname });
+});
+
+app.get("/browserFunctions.js", (req, res) => {
+  res.sendFile("/browserFunctions.js", { root: __dirname });
+});
+
+app.post("/reset", (req, res) => {
+  resetApp();
+  res.redirect("/");
 });
 
 //all files in these folders can be accessed with a GET request of the filename
-let assetFolders = ["styles","pages","images","images/buzzers","audio","fonts"];
+let assetFolders = ["styles", "pages", "images", "images/buzzers", "audio", "fonts"];
 assetFolders.forEach((folderName) => {
   let folderContents = fs.readdirSync(folderName);
   folderContents.forEach((fileName) => {
-    app.get("/"+fileName, (request, response) => {
-      response.sendFile(folderName+"/"+fileName, {root: __dirname });
+    app.get("/" + fileName, (req, res) => {
+      res.sendFile(folderName + "/" + fileName, { root: __dirname });
     });
   });
 });
--- browserFunctions.js
+++ browserFunctions.js
@@ -1,89 +1,132 @@
 var buzzerCache = new Map();
 
-function userInfoToClient(userInfo) {
-    for (const user in userInfo) {
-        re = new RegExp("\\W","g");
-        const userId = user.replaceAll(re,"_");
-        
-        
-        buzzerCache.set(userInfo[user].buzzerId, new Audio(userInfo[user].buzzerId+".wav"));
+function userInfoToClient(userInfo, isChargeMode, userChargeData) {
+  for (const user in userInfo) {
+    re = new RegExp("\\W", "g");
+    const userId = user.replaceAll(re, "_");
 
-        if ($("#userListPanel").find("#"+userId).length >= 1) {
-            //update buzzer icon of existing user
-            $("#userListPanel").find("#"+userId).find("img").attr("data-buzzerId",userInfo[user].buzzerId);
-            $("#userListPanel").find("#"+userId).find("img").attr("src", userInfo[user].buzzerId+".png");
-        }
-        else {
-            //new user
-            $("#"+userInfo[user].teamName+"List").find("ul").append("<li id="+userId+">"+"<img src="+userInfo[user].buzzerId+".png data-buzzerId="+userInfo[user].buzzerId+" class=userListBuzzerSelections>"+user+"<var></var></li>");
-        }
+    buzzerCache.set(userInfo[user].buzzerId, new Audio(`${userInfo[user].buzzerId}.wav`));
+
+    if ($("#userListPanel").find(`#${userId}`).length >= 1) {
+      //update buzzer icon of existing user
+      $("#userListPanel").find(`#${userId}`).find("img").attr("data-buzzerId", userInfo[user].buzzerId);
+      $("#userListPanel").find(`#${userId}`).find("img").attr("src", `${userInfo[user].buzzerId}.png`);
     }
+    else {
+      //new user
+      const buzzerIcon = `<img src=${userInfo[user].buzzerId}.png data-buzzerId=${userInfo[user].buzzerId} class=userListBuzzerSelections>`
+      const userText = `${user}${(isChargeMode ? `<var id=chargeBlocks>${getChargeBlocks(userChargeData[user].charges)}</var>` : "")}` // user name with charges, if applicable
+      $(`#${userInfo[user].teamName}List`)
+        .find("ul")
+        .append(
+          `<li id=${userId}>${buzzerIcon}${userText}<var id=${userId}></var></li>`
+        );
+    }
+  }
 }
 
-function gameStateToClient(currentTeam, currentScore) {
-    $("#PlayersList").css("opacity", 0.5);
-    $("#ChasersList").css("opacity", 0.5);
-    $("#"+currentTeam+"List").css("opacity", 1);
-    
-    $("#currentTeam").html(currentTeam+" turn");
-    $("#currentScore").html(currentScore);
+// creates visual representation of charges remaining for a user
+function getChargeBlocks(charges) {
+  let blockHtml = "";
+
+  for (let i = 3; i > 0; i--) {
+    blockHtml += charges >= i ? "■" : "";
+  }
+
+  return blockHtml;
 }
 
-function clearBuzzers() {
-    $("#firstBuzz").html("");
-    $("#userListPanel").find("var").html("");
+function gameStateToClient(currentTeam, currentScore, isChargeMode, userChargeData) {
+  $("#PlayersList").css("opacity", 0.5);
+  $("#ChasersList").css("opacity", 0.5);
+  $(`#${currentTeam}List`).css("opacity", 1);
+
+  // TODO: do i need this?
+  // if (isChargeMode && currentTeam == "Players") {
+  //   for (const user in userChargeData) {
+  //     const userEntry = userChargeData[user];
+  //     if (userEntry.charges == 0) {
+  //       toggleBuzzer(false);
+  //       toggleUserOpacity(user.replaceAll(" ", "_"), true);
+  //     }
+  //   }
+  // }
+
+  $("#currentTeam").html(`${currentTeam} Turn`);
+  $("#currentScore").html(currentScore);
+}
+
+function clearBuzzers(_, isChargeMode, userChargeData) {
+  $("#firstBuzz").html("");
+  for (const user in userChargeData) {
+    const userId = getUserId(user);
+    $("#userListPanel").find("#" + userId).find(`var#${userId}`).html("");
+    if (isChargeMode) {
+      $("#userListPanel").find("#" + userId).find("var#chargeBlocks").html(getChargeBlocks(userChargeData[user].charges));
+    };
+  }
 }
 
 function buzzInfoToClient(buzzInfo, soundOn) {
-    //play sound for first buzz in
-    if ($("#firstBuzz").html() == "") {
-        $("#firstBuzz").html(buzzInfo[0].userName);
-        const userId = buzzInfo[0].userName.replaceAll(" ","_");
-        let buzzerId = $("#userListPanel").find("#"+userId).find("img").attr("data-buzzerId");
-        let buzzerSound = buzzerCache.get(buzzerId);
-        if (soundOn == true) {
-            buzzerSound.play();
-        }
+  //play sound for first buzz in
+  if ($("#firstBuzz").html() == "") {
+    $("#firstBuzz").html(buzzInfo[0].userName);
+    const userId = getUserId(buzzInfo[0].userName);
+    let buzzerId = $("#userListPanel").find("#" + userId).find("img").attr("data-buzzerId");
+    let buzzerSound = buzzerCache.get(buzzerId);
+    if (soundOn == true) {
+      buzzerSound.play();
     }
+  }
 
-    for (let i=0; i < buzzInfo.length; i++) {
-        const userId = buzzInfo[i].userName.replaceAll(" ","_");
-        // $("#userListPanel").find("#"+userId).find("var").html(" ("+buzzInfo[i].lateTime/1000+")");
-        $("#userListPanel").find("#"+userId).find("var").html(" ["+buzzInfo[i].buzzOrder+"]");
-    }    
+  for (let i = 0; i < buzzInfo.length; i++) {
+    const userId = getUserId(buzzInfo[i].userName);
+    $("#userListPanel").find("#" + userId).find("var").last().html(" [" + buzzInfo[i].buzzOrder + "]");
+  }
 }
 
 function idkListToClient(idkList) {
-    idkList.forEach((userName) => {
-        const userId = userName.replaceAll(" ","_");
-        $("#userListPanel").find("#"+userId).find("var").html("<div style='display: inline; font-family: Times New Roman; font-size: 20px;'> ¯\\\_(ツ)_/¯</div>");
-    });
+  idkList.forEach((userName) => {
+    const userId = getUserId(userName);
+    $("#userListPanel").find("#" + userId).find("var").last().html("<div style='display: inline; font-family: Times New Roman; font-size: 20px;'> ¯\\\_(ツ)_/¯</div>");
+  });
 }
 
 function passToClient(teamName, soundOn) {
-    let passSound = new Audio("PikminDeath.wav");
-    if (soundOn == true) {
-        passSound.play();
-    }
-    $("#firstBuzz").html(teamName+" have Passed!");
+  let passSound = new Audio("PikminDeath.wav");
+  if (soundOn == true) {
+    passSound.play();
+  }
+  $("#firstBuzz").html(teamName + " have Passed!");
+  toggleBuzzer(false);
 }
-
-// function selectBuzzer(buzzerId) {
-//     if (buzzerId == "Mystery") {
-//         buzzerId = buzzerOptions[Math.floor(Math.random()*(buzzerOptions.length-1))];
-//     }
-//     let buzzerSound = new Audio(buzzerId+".wav");
-//     $("#buzzer").attr("src", buzzerId+".png" );
-//     buzzerSound.play();
-//     return buzzerSound;
-// }
 
 let objectionSound = new Audio("AAObjection.wav");
 function objectionToClient(userName) {
-    // objectionSound.play();
-    $("#objection").html("<img src='AAObjection.gif?"+Math.random()+"'><br>"+userName+" is objecting!<br>");
+  // objectionSound.play();
+  $("#objection").html("<img src='AAObjection.gif?" + Math.random() + "'><br>" + userName + " is objecting!<br>");
 }
 
 function clearObjectionToClient() {
-    $("#objection").html("");
+  $("#objection").html("");
+}
+
+function toggleBuzzer(isEnabled) {
+  $("#buzzer").css("opacity", isEnabled ? "1" : "0.5");
+  $("#buzzer").css("cursor", isEnabled ? "pointer" : "not-allowed");
+  buzzable = isEnabled;
+}
+
+function toggleIdk(isEnabled) {
+  $("#idkButton").css("opacity", isEnabled ? "1" : "0.5");
+  $("#idkButton").css("cursor", isEnabled ? "pointer" : "not-allowed");
+  idkButtonClickable = isEnabled;
+}
+
+function toggleUserOpacity(userId, isInactive) {
+  $(`li#${userId}`).css("opacity", isInactive ?  "0.5" : "1");
+}
+
+function getUserId(userName) {
+  return userName.replaceAll(" ", "_");
 }
